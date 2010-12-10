70646692d2b86b3f2a9064a364a04625d75d38b2
Stuart Carnie <stuart.carnie@gmail.com>
2010-12-10 10:46:23 -0700

    Script to push builds of Amiga to manomio.com

---
+++ push-amiga.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# variables
+DISTRIBUTION_CERTIFICATE='iPhone Distribution: Manomio LLC'
+PROVISIONING_PROFILE_PATH='/Users/stuartcarnie/Library/MobileDevice/Provisioning Profiles/iAmiga_Ad_Hoc.mobileprovision'
+ARCHIVE_FILENAME='iAmiga.zip'
+KEYCHAIN_LOCATION='/Users/stuartcarnie/Library/Keychains/login.keychain'
+KEYCHAIN_PASSWORD='sblaster'
+
+. build_ipa_short.sh
