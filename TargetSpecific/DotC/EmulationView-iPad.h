be593868820179c4dfc35aa7fe0eb173bbaefb29
Stuart Carnie <stuart.carnie@gmail.com>
2011-06-23 15:58:06 -0700

    Cleanup of dead code, added support for custom iPad view

---
+++ TargetSpecific/DotC/EmulationView-iPad.h
@@ -0,0 +1,16 @@
+//
+//  EmulationViewiPad.h
+//  iAmiga
+//
+//  Created by Stuart Carnie on 6/23/11.
+//  Copyright 2011 Manomio LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BaseEmulationViewController.h"
+
+@interface EmulationViewiPad : BaseEmulationViewController {
+    
+}
+
+@end
---
+++ TargetSpecific/DotC/EmulationView-iPad.m
@@ -0,0 +1,20 @@
+//
+//  EmulationViewiPad.m
+//  iAmiga
+//
+//  Created by Stuart Carnie on 6/23/11.
+//  Copyright 2011 Manomio LLC. All rights reserved.
+//
+
+#import "EmulationView-iPad.h"
+
+
+@implementation EmulationViewiPad
+
+#pragma mark - View lifecycle
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    return UIInterfaceOrientationIsLandscape(interfaceOrientation);
+}
+
+@end
