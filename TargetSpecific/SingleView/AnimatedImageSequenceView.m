f140a9068e18137e81f84b751921f28747efa96b
Stuart Carnie <stuart.carnie@gmail.com>
2011-06-24 11:27:19 -0700

    UPDATE - add intro sequence for iPhone and user guide web view

---
+++ TargetSpecific/SingleView/AnimatedImageSequenceView.m
@@ -0,0 +1,102 @@
+//
+//  AnimatedImageSequenceView.m
+//  iAmiga
+//
+//  Created by Stuart Carnie on 6/23/11.
+//  Copyright 2011 Manomio LLC. All rights reserved.
+//
+
+#import "AnimatedImageSequenceView.h"
+
+@implementation FadeAction
+
+@synthesize fadeInTime=_fadeInTime, holdTime=_holdTime, imageName=_imageName, fadeOutTime=_fadeOutTime;
+
++ (FadeAction *)actionWithFadeIn:(NSTimeInterval)fadeIn holdTime:(NSTimeInterval)holdTime fadeOut:(NSTimeInterval)fadeOut forImageNamed:(NSString *)imageName {
+    return [[[FadeAction alloc] initWithFadeIn:fadeIn holdTime:holdTime fadeOut:fadeOut forImageNamed:imageName] autorelease];
+}
+
+- (id)initWithFadeIn:(NSTimeInterval)fadeIn holdTime:(NSTimeInterval)holdTime fadeOut:(NSTimeInterval)fadeOut forImageNamed:(NSString *)imageName {
+    self = [super init];
+    if (!self) return nil;
+    
+    _fadeInTime = fadeIn;
+    _holdTime = holdTime;
+    _fadeOutTime = fadeOut;
+    self.imageName = imageName;
+    
+    return self;
+}
+
+@end
+
+@interface AnimatedImageSequenceView()
+
+- (void)animateNext;
+
+@end
+
+
+@implementation AnimatedImageSequenceView
+
+@synthesize sequence=_sequence, delegate;
+
+- (id)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (!self) return nil;
+    
+    self.opaque = YES;
+    self.backgroundColor = [UIColor blackColor];
+    
+    UIImageView *view = [[[UIImageView alloc] initWithFrame:frame] autorelease];
+    view.opaque = NO;
+    view.backgroundColor = [UIColor clearColor];
+    view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [self addSubview:view];
+    
+    view = [[[UIImageView alloc] initWithFrame:frame] autorelease];
+    view.opaque = NO;
+    view.backgroundColor = [UIColor clearColor];
+    view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [self addSubview:view];
+    
+    return self;
+}
+
+- (void)startWithSequence:(NSMutableArray *)sequence {
+    self.sequence = sequence;
+    [self animateNext];
+}
+
+- (void)animateNext {
+    if ([_sequence count] == 0) {
+        [self.delegate sequenceDidFinishForView:self];
+        return;
+    }
+    
+    FadeAction *next = [_sequence objectAtIndex:0];
+    NSTimeInterval holdTime = next.holdTime;
+    NSTimeInterval fadeInTime = next.fadeInTime;
+    
+    UIImageView *bottom = [self.subviews objectAtIndex:0];
+    bottom.alpha = 0.0f;
+    bottom.image = [UIImage imageNamed:next.imageName];
+    [_sequence removeObjectAtIndex:0];    
+    
+    UIImageView *top = [self.subviews objectAtIndex:1];
+    
+    [UIView animateWithDuration:fadeInTime 
+                     animations:^(void) {
+                         top.alpha = 0.0f;
+                         bottom.alpha = 1.0f;
+                     } completion:^(BOOL finished) {
+                         [self insertSubview:bottom aboveSubview:top];
+                         [self performSelector:@selector(animateNext) withObject:nil afterDelay:holdTime];
+                     }];
+}
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+@end
