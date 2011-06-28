8a1c18108c559b3756739f341885595706912929
Stuart Carnie <stuart.carnie@gmail.com>
2011-06-27 23:32:57 -0700

    UPDATE - new chrome / UI for iPad

--- TargetSpecific/DotC/EmulationView-iPad.m
+++ TargetSpecific/DotC/EmulationView-iPad.m
@@ -10,6 +10,10 @@
 
 
 @implementation EmulationViewiPad
+@synthesize menuView;
+@synthesize menuButton;
+@synthesize closeButton;
+@synthesize mouseHandler;
 
 #pragma mark - View lifecycle
 
@@ -17,4 +21,54 @@
     return UIInterfaceOrientationIsLandscape(interfaceOrientation);
 }
 
+- (void)dealloc {
+    [menuButton release];
+    [closeButton release];
+    [menuView release];
+    [mouseHandler release];
+    [super dealloc];
+}
+- (void)viewDidUnload {
+    [self setMenuButton:nil];
+    [self setCloseButton:nil];
+    [self setMenuView:nil];
+    [self setMouseHandler:nil];
+    [super viewDidUnload];
+}
+- (IBAction)hideMenu:(id)sender {
+    menuButton.hidden = NO;
+    closeButton.hidden = YES;
+    [UIView animateWithDuration:0.500f 
+                     animations:^(void) {
+                         CGRect frame = menuView.frame;
+                         frame.origin.y = -frame.size.height;
+                         menuView.frame = frame;                         
+                     } completion:^(BOOL finished) {
+                         [self resumeEmulator];
+                         mouseHandler.userInteractionEnabled = YES;
+                     }];
+
+}
+
+- (IBAction)showMenu:(id)sender {
+    [self pauseEmulator];
+    menuButton.hidden = YES;
+    closeButton.hidden = NO;
+    menuView.hidden = NO;
+    mouseHandler.userInteractionEnabled = NO;
+    
+    NSString *userGuidePath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/UserGuide.html"];
+    if (![[NSFileManager defaultManager] fileExistsAtPath:userGuidePath]) {
+        userGuidePath = [[NSBundle mainBundle] pathForResource:@"UserGuide" ofType:@"html"];
+    }
+    
+    NSURLRequest *req = [NSURLRequest requestWithURL:[NSURL fileURLWithPath:userGuidePath]];
+	//[webView loadRequest:req];
+    
+    [UIView animateWithDuration:0.500f animations:^(void) {
+        CGRect frame = menuView.frame;
+        frame.origin.y = 0;
+        menuView.frame = frame;
+    }];
+}
 @end
