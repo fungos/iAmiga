83289b2885a916514e7e99d81310449ef79fab48
Stuart Carnie <stuart.carnie@gmail.com>
2011-06-27 23:32:57 -0700

    UPDATE - new chrome / UI for iPad

--- TargetSpecific/DotC/EmulationView-iPad.h
+++ TargetSpecific/DotC/EmulationView-iPad.h
@@ -11,6 +11,16 @@
 
 @interface EmulationViewiPad : BaseEmulationViewController {
     
+    UIButton *menuButton;
+    UIButton *closeButton;
+    UIView *mouseHandler;
+    UIView *menuView;
 }
+- (IBAction)hideMenu:(id)sender;
+- (IBAction)showMenu:(id)sender;
+@property (nonatomic, retain) IBOutlet UIView *menuView;
+@property (nonatomic, retain) IBOutlet UIButton *menuButton;
+@property (nonatomic, retain) IBOutlet UIButton *closeButton;
+@property (nonatomic, retain) IBOutlet UIView *mouseHandler;
 
 @end
